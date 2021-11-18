5c49dfed186dea68d6c3323f0bd656e139d3d823
Warstelide49 <evindrenji01@gmail.com>
2021-11-18 11:53:50 +0530

    functional

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 my_library=[];
 
+buttons=[];
+delete_btns=[];
+all_cards=[];
+
 submitter=document.querySelector('button');
 submitter.addEventListener('click',add_book_to_library);
 
@@ -23,16 +27,30 @@ function add_book_to_library(){
 	let book=new Book(form_title.value,form_author.value,form_pages.value,form_read.checked);
 	my_library.push(book);
 	card=document.createElement('div');
+	all_cards.push(card);
 	card.classList.add('card');
 	for(property in book){
 		if(property=="read"){
-			button=document.createElement('button')
+			button=document.createElement('button');
 			card.appendChild(button);
-			button.textContent="Mark Complete";
-			if(book[property]===true){
+			button.textContent=(book[property]===true) ? "Mark Incomplete" : "Mark Complete";
+			if (book[property]===true)
 				card.classList.add('read');
-				button.textContent="Mark Incomplete";
-			}
+			buttons.push(button);
+			fix_listener();
+			/*button.addEventListener('click',()=>{
+				if(book[property]===false){
+					card.classList.add('read');
+					button.textContent="Mark Incomplete";
+					book[property]=true;
+				}
+				else{
+					card.classList.remove('read');
+					button.textContent="Mark Complete";
+					book[property]=false;
+				}
+			});*/
+			
 		}
 		else{
 			element=document.createElement('div');
@@ -42,8 +60,47 @@ function add_book_to_library(){
 		}
 		
 	}
+	delete_btn=document.createElement('button');
+	card.appendChild(delete_btn);
+	delete_btn.textContent="Delete";
+	delete_btns.push(delete_btn);
+
+	delete_btns.forEach((btn)=>{
+		btn.addEventListener('click',delete_card);
+	})
+
 	card_holder.appendChild(card);
+
+
 	form_title.value='';
 	form_author.value='';
 	form_pages.value='';
+}
+
+function fix_listener(){
+	buttons.forEach((btn)=>{
+		btn.addEventListener('click',button_purpose);
+	});
+}
+
+function button_purpose(event){
+	if(my_library[buttons.indexOf(event.target)][property]==false){
+		all_cards[buttons.indexOf(event.target)].classList.add('read');
+		event.target.textContent="Mark Incomplete";
+		my_library[buttons.indexOf(event.target)][property]=true;
+	}
+	else{
+		all_cards[buttons.indexOf(event.target)].classList.remove('read');
+		event.target.textContent="Mark Complete";
+		my_library[buttons.indexOf(event.target)][property]=false;
+	}
+}
+
+function delete_card(event){
+	required_index=delete_btns.indexOf(event.target);
+	all_cards[required_index].remove();
+	all_cards.splice(required_index,1);
+	buttons.splice(required_index,1);
+	my_library.splice(required_index,1);
+	delete_btns.splice(required_index,1);
 }
